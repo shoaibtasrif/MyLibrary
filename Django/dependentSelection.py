915c97a31506dd28258026102206c8defcdb6b81
Shoaib tasrif <shoaib01@student.sust.edu>
2019-09-22 00:02:42 +0600

    m

--- Django/dependentSelection.py
+++ Django/dependentSelection.py
@@ -53,6 +53,18 @@ class PersonForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.fields['city'].queryset = City.objects.none()
         # print('hi')
+        print(type(self.data))
+        for k1 in self.data:  # iterating dictionary
+            print(k1, self.data.get(k1, None))
+
+        '''
+        <class 'django.http.request.QueryDict'>
+        csrfmiddlewaretoken 8ifH6Xed7mhcxTKRazO7KKfEp4dhmfgFJLIrbdlBqf6B98GcgP9eoKyJ61BsEbxT
+        name gege
+        birthdate 2016-01-01
+        country 2
+        city 3
+        '''
 
         if 'country' in self.data:
             try:
@@ -60,7 +72,7 @@ class PersonForm(forms.ModelForm):
                 self.fields['city'].queryset = City.objects.filter(
                     country_id=country_id).order_by('name')
             except (ValueError, TypeError):
-                pass  # invalid input from the client; ignore and fallback to empty City queryset
+                pass
         elif self.instance.pk:
             self.fields['city'].queryset = self.instance.country.city_set.order_by(
                 'name')
@@ -92,3 +104,81 @@ urlpatterns = [
     path('<int:pk>/', PersonUpdateView.as_view(), name='person_change'),
     path('ajax/load-cities/', load_cities, name='ajax_load_cities'),
 ]
+
+'''
+-> load_cities.html
+
+<html>
+<body>
+
+    <option value="">---------</option>
+    {% for city in cities %}
+    <option value="{{ city.pk }}">{{ city.name }}</option>
+    {% endfor %}
+</body>
+</html>
+
+
+-> person_create.html
+
+{% extends 'base.html' %}
+
+
+{% block content_1 %}
+<title>Create Person </title>
+{% endblock content_1 %}
+
+
+{% block content_2 %}
+<h2>Person Form</h2>
+
+<form method="post" id="personForm" data-cities-url="{% url 'ajax_load_cities' %}" novalidate>
+    {% csrf_token %}
+    <table>
+        {{ form.as_table }}
+    </table>
+    <button type="submit">Save</button>
+    <a href="{% url 'person_changelist' %}">Nevermind</a>
+</form>
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script>
+    $("#id_country").change(function () {
+        var url = $("#personForm").attr("data-cities-url");  // get the url of the `load_cities` view
+        var countryId = $(this).val();  // get the selected country ID from the HTML input
+
+        $.ajax({                       // initialize an AJAX request
+            url: url,                   // set the url of the request (= localhost:8000/hr/ajax/load-cities/)
+            data: {
+                'country': countryId       // add the country id to the GET parameters
+            },
+            success: function (shoaib) {   // `data` is the return of the `load_cities` view function
+                $("#id_city").html(shoaib);  // replace the contents of the city input with the data that came from the server
+            }
+        });
+    });
+</script>
+{% endblock content_2 %}
+
+->person_list.html
+{% extends 'base.html' %}
+
+
+{% block content_1 %}
+<title>Person List</title>
+
+{% endblock content_1 %}
+
+
+{% block content_2 %}
+
+{% for p in people %}
+<a href="{{p.id}}/">{{p.name}}</a>
+{% endfor %}
+
+<a href="add/"> Add New Person </a>
+
+{% endblock content_2 %}
+
+
+'''
